d9dc5fe10313a34907e17337911fb1708da2d94e
Xaver Himmelsbach <xaver.himmelsbach@gmail.com>
2022-10-11 16:36:10 +0200

    Add WriteImage function to utils

--- utils/imageUtils.go
+++ utils/imageUtils.go
@@ -1,10 +1,14 @@
 package utils
 
 import (
+	"fmt"
 	"image"
+	"image/jpeg"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
 	"os"
+	"path"
 )
 
 // ReadImage Takes the path to an image file in the file system and returns the decoded image
@@ -17,4 +21,28 @@ func ReadImage(path string) (img image.Image, err error) {
 	defer file.Close()
 	img, _, err = image.Decode(file)
 	return img, err
+}
+
+// WriteImage encodes an image as either PNG or JPEG according to the file extension and writes it to the provided path
+func WriteImage(filePath string, img image.Image) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	extension := path.Ext(filePath)
+	switch extension {
+	case ".jpg":
+		fallthrough
+	case ".jpeg":
+		err = jpeg.Encode(file, img, nil)
+	case ".png":
+		err = png.Encode(file, img)
+	default:
+		err = fmt.Errorf("unknown file extension: %q", extension)
+	}
+
+	return err
 }
