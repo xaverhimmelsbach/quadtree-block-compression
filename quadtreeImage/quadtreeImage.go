c0540e330ad39fd120ea8bb51f632df624956b33
Xaver Himmelsbach <xaver.himmelsbach@gmail.com>
2022-10-11 15:26:10 +0200

    Add prints to quadtreeImage methods

--- quadtreeImage/quadtreeImage.go
+++ quadtreeImage/quadtreeImage.go
@@ -1,20 +1,25 @@
 package quadtreeImage
 
+import (
+	"fmt"
+	"image"
+)
+
 type QuadtreeImage struct {
 	BaseImage image.Image
 }
 
 // TODO: Implement
 func (*QuadtreeImage) Partition() {
-
+	fmt.Println("Partitioning QuadtreeImage")
 }
 
 // TODO: Implement
 func (*QuadtreeImage) Encode() {
-
+	fmt.Println("Encoding QuadtreeImage")
 }
 
 // TODO: Implement
 func (*QuadtreeImage) WriteFile(path string) {
-
+	fmt.Printf("Writing QuadtreeImage to %q\n", path)
 }
