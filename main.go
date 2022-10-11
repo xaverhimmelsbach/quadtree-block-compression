0b29292a4f61fe2a1069ff73ffc442af22f3fd5c
Xaver Himmelsbach <xaver.himmelsbach@gmail.com>
2022-10-11 15:26:39 +0200

    Add basic program flow

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"image"
+
+	"github.com/xaverhimmelsbach/quadtree-block-compression/quadtreeImage"
+)
+
+// TODO: Implement
+func readImage(path string) (image image.Image, err error) {
+	return image, err
+}
 
 func main() {
-	fmt.Println("Quadtree Block Compression")
+	// Parse arguments
+	inputPath := flag.String("input", "", "Image to encode as quadtree")
+	outputPath := flag.String("output", "", "Path to write encoded file to")
+	flag.Parse()
+
+	// Read image from file system
+	image, err := readImage(*inputPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create quadtree image representation
+	quadtreeImage := quadtreeImage.QuadtreeImage{
+		BaseImage: image,
+	}
+
+	// Partition image into a quadtree structure
+	quadtreeImage.Partition()
+
+	// Encode quadtree structure
+	quadtreeImage.Encode()
+
+	// Write quadtree structure to output
+	quadtreeImage.WriteFile(*outputPath)
+
+	fmt.Printf("Encoded %q as a quadtree image and wrote it to %q", *inputPath, *outputPath)
 }
